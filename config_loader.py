0b312544509db2325368dddd9d68e0fc2e1ad8dc
kian <kiannamdari51@gmail.com>
2026-07-06 05:43:26 +0330

    on branch develop fix: fixs merge conflicts in config_loader.py

--- config_loader.py
+++ config_loader.py
@@ -8,20 +8,11 @@ def config_loader(filename="config.yaml", search_paths=None):
             os.getcwd(),
             os.path.dirname(os.path.abspath(__file__)),
         ]
-<<<<<<< HEAD
 
     for path in search_paths:
         full_path = os.path.join(path, filename)
         if os.path.isfile(full_path):
             with open(full_path, "r", encoding="utf-8") as f:
-=======
-       
-    for path in search_paths:
-        full_path = os.path.join(path, filename)
-        print(full_path)
-        if os.path.isfile(full_path):
-            with open(full_path, "r", encoding="UTF-8") as f:
->>>>>>> feat_two_player_rps
                 data = yaml.safe_load(f)
             return data if data else {}
 
@@ -29,9 +20,4 @@ def config_loader(filename="config.yaml", search_paths=None):
 
 
 
-<<<<<<< HEAD
 config = config_loader("config.yaml")
-=======
-config = config_loader("config.yaml")
-print(config)
->>>>>>> feat_two_player_rps
