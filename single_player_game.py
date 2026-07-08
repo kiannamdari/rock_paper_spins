5bd6b46d9d88dfa98434b6833ccdbcbaf2fcd136
kian <kiannamdari51@gmail.com>
2026-07-08 12:29:41 +0330

    on branch work_with_file wip:feat: add json files and change single_player_game.py

--- single_player_game.py
+++ single_player_game.py
@@ -1,6 +1,6 @@
 #!/home/tavaneshkade/workspace/rock_paper_spins/.venv/bin/python3
 import random
-
+import json
 from config_loader import config
 
 def single_player_game():
@@ -10,75 +10,106 @@ def single_player_game():
         print(key,':', value)
     player_score = 0
     bot_score = 0
+    game = {
+        "round_one": {},
+        "round_two": {},
+        "round_three": {}
+    }
     while True:
         bot_choice = random.choice(bot_options)
 
-        if bot_score == config.get('score').get('total_score'):
-            print('bot win!!!!!!!!!!!!')
+        if  player_score != config.get("score").get("total_score"):
+            choice = (input('Choose your number in options: '))
+
+            if choice not in options:
+                print('choose a variable in options')
+                continue
+
+            if choice == 'score':
+                print('your score is', player_score)
+                print('bot score is', bot_score)
+                continue
+
+            print('your choose is', config.get('options').get(choice))
+            print('bot choose is', bot_choice)
+
+            if choice == '1' and bot_choice == 'rock':
+                print('tie')
+
+
+            elif choice == '1' and bot_choice == 'paper':
+                print('you lose')
+                bot_score += 1
+
+
+            elif choice == '1' and bot_choice == 'scissors':
+                print('you win')
+                player_score += 1
+
+
+            elif choice == '2' and bot_choice == 'rock':
+                print('you win')
+                player_score += 1
+
+
+            elif choice == '2' and bot_choice == 'paper':
+                print('tie')
+
+
+            elif choice == '2' and bot_choice == 'scissors':
+                print('you lose')
+                bot_score += 1
+
+
+            elif choice == '3' and bot_choice == 'rock':
+                print('you lose')
+                bot_score += 1
+
+
+            elif choice == '3' and bot_choice == 'paper':
+                print('you win')
+                player_score += 1
+
+            elif choice == '3' and bot_choice == 'scissors':
+                print('tie')
+
+            if bot_score == 1 or player_score == 1:
+                round_one = {
+                    "player_choice": choice,
+                    "bot_choice": bot_choice,
+                    "player_score": player_score,
+                    "bot_score": bot_score
+                }
+                game.update({"round_one": round_one})
+            if bot_score == 2 or player_score == 2:
+                round_two = {
+                    "player_choice": choice,
+                    "bot_choice": bot_choice,
+                    "player_score": player_score,
+                    "bot_score": bot_score
+                }
+                game.update({"round_two": round_two})
+            if bot_score == 3 or player_score == 3:
+                round_three = {
+                "player_choice": choice,
+                "bot_choice": bot_choice,
+                "player_score": player_score,
+                "bot_score": bot_score
+                }
+                game.update({"round_three": round_three})
+            continue
+        elif player_score == config.get("score").get("total_score"):
+            print("you are winner !!!!!!!!!!!!")
             break
 
-        elif player_score == config.get('score').get('total_score'):
-            print('you win!!!!!!!!!!!!')
+        elif bot_score == config.get("score").get("total_score"):
+            print("bot is winner !!!!!!!!!!!!!")
             break
-        choice = (input('Choose your number in options: '))
 
-        if choice not in options:
-            print('choose a variable in options')
-            continue
-
-        if choice == 'score':
-            print('your score is', player_score)
-            print('bot score is', bot_score)
-            continue
-        print('your choose is', config.get('options').get(choice))
-        print('bot choose is', bot_choice)
-
-        if choice == '1' and bot_choice == 'rock':
-            print('tie')
-            continue
-
-        elif choice == '1' and bot_choice == 'paper':
-            print('you lose')
-            bot_score += 1
-            continue
-
-        elif choice == '1' and bot_choice == 'scissors':
-            print('you win')
-            player_score += 1
-            continue
-
-        elif choice == '2' and bot_choice == 'rock':
-            print('you win')
-            player_score += 1
-            continue
-
-        elif choice == '2' and bot_choice == 'paper':
-            print('tie')
-            continue
-
-        elif choice == '2' and bot_choice == 'scissors':
-            print('you lose')
-            bot_score += 1
-            continue
-
-        elif choice == '3' and bot_choice == 'rock':
-            print('you lose')
-            bot_score += 1
-            continue
-
-        elif choice == '3' and bot_choice == 'paper':
-            print('you win')
-            player_score += 1
-            continue
-
-        elif choice == '3' and bot_choice == 'scissors':
-            print('tie')
-            continue
+    print(game)
+    json_data = json.dumps(game, indent=20)
+    with open("works_of_single_player_game.json", "w" , encoding="utf-8") as f:
+        f.write(json_data)
 
 if __name__ == '__main__':
     single_player_game()
-
-
-
-
-
